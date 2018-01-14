c9239d9ec3322b1304f0e609885056d5adc625ad
AlexMidjich <alexander@midjich.se>
2018-01-14 23:36:03 +0100

    added a slider to hero part

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,31 @@
+let slideIndex = 0;
+showSlides(slideIndex);
 
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName('slide');
+  const dots = document.getElementsByClassName('dot')
+
+  for (i = 0; i < slides.length; i++){
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace( ' active', '');
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex-1].style.display = 'block';
+  dots[slideIndex-1].className += ' active';
+  setTimeout(showSlides, 6000);
+}
+
+// $( document ).ready(function() {
+//   console.log('hej');
+//   const slide = $('slide');
+//   console.log(slide);
+//     $('.dot').click( function() {
+//       $('.slide')[0].addClass('active')
+//     });
+// });
