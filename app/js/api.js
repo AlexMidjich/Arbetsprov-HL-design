e13126c8e8e3d98ace89637cd26eb8a98d53e1ea
AlexMidjich <alexander@midjich.se>
2018-01-15 20:51:08 +0100

    new search results will now replace product list each time

--- app/js/api.js
+++ app/js/api.js
@@ -6,9 +6,9 @@ $( document ).ready(function() {
     $.getJSON('https://webshop.wm3.se/api/v1/shop/products/search.json?q=' + product + '&media_file=true',
     function(data) {
       var i;
+      $('.product-box').remove();
       for (i = 0; i < data.products.length; i++) {
-        $('#img_' + [i]).attr('src', data.products[i].media_file.url_small);
-        $('#text_' + [i]).html(data.products[i].name);
+        $('.products-wrapper').append('<div class="product-box"><div class="product-img"><img src='+ data.products[i].media_file.url_small + ' id=img_' + [i] + ' /></div><p id=text_' + [i] + '>' + data.products[i].name + '</p></div>');
       }
     });
   });
